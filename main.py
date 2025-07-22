2c9544071b03706a7b48ab0f9b5543b5499d1c36
Silverblooded <Bladehornsby@live.com>
2025-07-22 09:52:19 -0500

    B working

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
+file_contents = ""
+num_words = None
+
+
+def get_book_text(file_contents):
+    words = 0
+    for words in file_contents:
+        words += 1
+        num_words = words
+        return num_words
+
 
 with open("./books/frankenstein.txt") as f:
     file_contents = f.read()
-    print(file_contents)
-
-def get_book_text():
-    pass
-
+    file_list = file_contents.split(" ")
+    get_book_text(file_list)
+    print(f"{num_words} words found in the document")
