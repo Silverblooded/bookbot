2a95e17decf137ad9681c027689c9cea0a5ebc05
Silverblooded <Bladehornsby@live.com>
2025-07-22 11:30:01 -0500

    I did it, don't ask how it just clicked

--- main.py
+++ main.py
@@ -1,17 +1,12 @@
-file_contents = ""
-num_words = None
+def count():
+    with open("./books/frankenstein.txt") as f:
+        num_words = 0
+        file_contents = f.read()
+        book = file_contents.split(" ")
+        for i in range(len(book)):
+            num_words = i
+        print(f"{num_words} words found in the document")
+    # print(f"{num_words} words found in the document")
 
 
-def get_book_text(file_contents):
-    words = 0
-    for words in file_contents:
-        words += 1
-        num_words = words
-        return num_words
-
-
-with open("./books/frankenstein.txt") as f:
-    file_contents = f.read()
-    file_list = file_contents.split(" ")
-    get_book_text(file_list)
-    print(f"{num_words} words found in the document")
+count()
