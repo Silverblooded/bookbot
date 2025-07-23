b266a63b170eb9c52c8e2e5f642ed9acd99b5a1b
Silverblooded <Bladehornsby@live.com>
2025-07-23 09:15:40 -0500

    split count to sub-file

---
+++ stats.py
@@ -0,0 +1,10 @@
+def count():
+    with open("./books/frankenstein.txt") as f:
+        num_words = 0
+        file_contents = f.read()
+        book = file_contents.split(" ")
+        num_words = len(file_contents.split())
+        print(f"{num_words} words found in the document")
+
+
+count()
