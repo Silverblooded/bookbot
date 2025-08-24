b64c6fc2778bf6b2bcb0d835766e7fecb56e7b56
Silverblooded <Bladehornsby@live.com>
2025-08-24 14:06:35 -0500

    pro movement

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 from stats import count, characters_sorted, sort_on
+import sys
+
+
+def open():
+    if len(sys.argv) < 1:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+        return
